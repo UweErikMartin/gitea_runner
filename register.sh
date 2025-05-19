1dee0511a36e04c04ffbcecd85a4e92d67df9e4f
Uwe-Erik Martin <uwe_erik_martin@yahoo.de>
2025-05-19 18:15:51 +0200

    read the registration tokens from an environment variable with the Runner name

--- register.sh
+++ register.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+token_name=$(echo ${GITEA_RUNNER_NAME^^} | tr "-" "_")_REGISTRATION_TOKEN
 echo "Runner not registered, registering now..."
 act_runner register -c /etc/act_runner/config.yaml \
   --no-interactive \
   --instance $GITEA_INSTANCE \
   --name $GITEA_RUNNER_NAME \
   --labels $GITEA_RUNNER_LABELS \
-  --token $GITEA_RUNNER_REGISTRATION_TOKEN
+  --token ${!token_name}
 chown -v -R act_runner:act_runner /var/lib/act_runner
