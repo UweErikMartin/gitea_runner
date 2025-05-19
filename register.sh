fc39c2d6aade43b106d30bd3962876490a926bbd
Uwe-Erik Martin <uwe_erik_martin@yahoo.de>
2025-05-19 20:25:53 +0200

    take labels from config file

--- register.sh
+++ register.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ -f $GITEA_CONFIG_PATH/$GITEA_RUNNER_NAME ]; then
   cp -v $GITEA_CONFIG_PATH/$GITEA_RUNNER_NAME /var/lib/act_runner/.runner
-  cp -v $GITEA_CONFIG_PATH/$GITEA_RUNNER_NAME.config.yaml /etc/act_runner/config.yaml
   chown -v -R act_runner:act_runner /var/lib/act_runner
 else
   echo "Runner not registered, registering now..."
@@ -13,7 +12,6 @@ else
   chown -v -R act_runner:act_runner /var/lib/act_runner
   if [ -d $GITEA_CONFIG_PATH ]; then
     cp -v /var/lib/act_runner/.runner $GITEA_CONFIG_PATH/$GITEA_RUNNER_NAME
-    cp -v /etc/act_runner/config.yaml $GITEA_CONFIG_PATH/$GITEA_RUNNER_NAME.config.yaml
   fi
 fi
 
